78f0af496d731686084a9e3f7ea378350dc32993
GitHub Action <action@github.com>
2025-08-11 19:28:12 +0000

    Initialize template repository proper
    Set package name to com.github.gamerpro329.wakboots
    Set project name to wakboots
    Set modid to wakboots

--- src/main/java/com/github/gamerpro329/wakboots/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/gamerpro329/wakboots/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.gamerpro329.wakboots.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/gamerpro329/wakboots/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/gamerpro329/wakboots/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.gamerpro329.wakboots.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
